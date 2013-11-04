jeroendesloovere/fork-cms-module-litters

commit ef7d0a19bdb5c669f832d5764ae379339bccafae

URL callback now working - fixes #5

backend/modules/litters/layout/templates/edit.tpl
@@ -10,8 +10,7 @@
 {$txtName} {$txtNameError}
 	<div id="pageUrl">
 		<div class="oneLiner">
-			{option:detailURL}<p><span><a href="{$detailURL}">{$detailURL}/<span id="generatedUrl"></span></a></span>
-				</p>{/option:detailURL}
+			{option:detailURL}<p><span><a href="{$detailURL}">{$detailURL}/<span id="generatedUrl">{$item.url}</span></a></span></p>{/option:detailURL}
 			{option:!detailURL}<p class="infoMessage">{$errNoModuleLinked}</p>{/option:!detailURL}
 		</div>
 	</div>

backend/modules/litters/actions/edit.php
@@ -106,6 +106,9 @@
 			// parse additional variables
 			if($url404 != $url) $this->tpl->assign('detailURL', SITE_URL . $url);
 
+			// fetch proper slug
+			$this->record['url'] = $this->meta->getURL();
+
 			$this->tpl->assign('item', $this->record);
 			$datagrid = $this->dgYoungs->getContent();
 			if($datagrid !== null) $this->tpl->assign('dgYoungs', $datagrid);

backend/modules/litters/engine/model.php
@@ -99,10 +99,12 @@ EOQ;
 			if($table != 'litters_youngs'){
 				$req = <<< EOQ
 SELECT
-	*,
-	UNIX_TIMESTAMP(birth_date) AS birth_date
-FROM ${table}
-WHERE id = ?;
+	t.*,
+	meta.url,
+	UNIX_TIMESTAMP(t.birth_date) AS birth_date
+FROM ${table} AS t
+LEFT JOIN meta ON t.meta_id = meta.id
+WHERE t.id = ?;
 EOQ;
 			}
 			else{

backend/modules/litters/js/litters.js
@@ -1,0 +1,15 @@
+if(!jsBackend){
+	var jsBackend = new Object();
+}
+
+jsBackend.litters = {
+	init: function(){
+		// set up meta field
+		if($('#name').length > 0) $('#name').doMeta();
+	},
+
+	// end
+	eoo: true
+}
+
+$(jsBackend.litters.init);

commit 4d6edaf7875f0f59e05c2c27aa0972850d160e38

Fixes a bug introduced in ef7d0a19bdb5c669f832d5764ae379339bccafae: litter link (URL) in edit action was missing the last part (displayed correctly but href was incomplete).

backend/modules/litters/layout/templates/edit.tpl
@@ -10,7 +10,7 @@
 {$txtName} {$txtNameError}
 	<div id="pageUrl">
 		<div class="oneLiner">
-			{option:detailURL}<p><span><a href="{$detailURL}">{$detailURL}/<span id="generatedUrl">{$item.url}</span></a></span></p>{/option:detailURL}
+			{option:detailURL}<p><span><a href="{$detailURL}/{$item.url}">{$detailURL}/<span id="generatedUrl">{$item.url}</span></a></span></p>{/option:detailURL}
 			{option:!detailURL}<p class="infoMessage">{$errNoModuleLinked}</p>{/option:!detailURL}
 		</div>
 	</div>